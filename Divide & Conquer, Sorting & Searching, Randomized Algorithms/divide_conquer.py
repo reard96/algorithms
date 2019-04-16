d08d1593436de5695a851d4b9835238a93d6091d
Dan Reardon <reardon.danielj@gmail.com>
2019-04-16 16:29:49 -0400

    assignment 2 complete

--- Divide & Conquer, Sorting & Searching, Randomized Algorithms/divide_conquer.py
+++ Divide & Conquer, Sorting & Searching, Randomized Algorithms/divide_conquer.py
@@ -3,10 +3,39 @@ def get_arr(text):
     '''
     with open(text) as file:
         arr = [int(s) for s in file.read().split('\n') if s.isdigit()]
-        print(len(arr)) # Should be 100,000 for the assignment
         return arr
 
 
-def inversions(array):
+def inversions(arr):
     ''' Take in array of ints and compute the number of inversions
     '''
+    if len(arr) == 1:
+        return arr, 0
+
+    else:
+        left = arr[:len(arr) // 2]
+        right = arr[len(arr) // 2:]
+
+        left, left_length = inversions(left)
+        right, right_length = inversions(right)
+        sorted = []
+
+        i = 0
+        j = 0
+        count = 0 + left_length + right_length
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            sorted.append(left[i])
+            i += 1
+        else:
+            sorted.append(right[j])
+            j += 1
+            count += (len(left) - i)
+
+    sorted += left[i:]
+    sorted += right[j:]
+
+    return sorted, count
+
+print(inversions(get_arr('integer_array.txt'))[1])
